62480857d2534e1de0af6898b7da9efaaf71e1c3
dorian-palace <dorian.palace@laplateforme.io>
2022-11-08 14:16:11 +0000

    job03

---
+++ jour01/job03/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 2;  
+
+    printf("%d\n", a / b);
+    printf("%d\n", a  + b);
+    printf("%d\n", a  - b);
+    printf("%d\n", a  * b);
+
+    return 0;
+}
