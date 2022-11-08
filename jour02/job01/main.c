a2d912752aac6afe7ab570033c60137538488af5
dorian-palace <dorian.palace@laplateforme.io>
2022-11-08 16:08:44 +0000

    j 2 job 1

---
+++ jour02/job01/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+void my_mul()
+{
+    int a;
+    int b;
+    int result;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    result = a * b; 
+
+    printf("%d", result);
+}
+
+void my_add()
+{
+    int a;
+    int b;
+    int result;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    result = a + b; 
+
+    printf("%d", result); 
+}
+
+void my_sub()
+{
+    int a;
+    int b;
+    int result;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    result = a - b; 
+
+    printf("%d", result);
+}
+
+void my_div()
+{
+    int a;
+    int b;
+    int result;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    result = a / b; 
+
+    printf("%d", result);
+}
+
+void my_mod()
+{
+    int a;
+    int b;
+    int result;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    result = a % b; 
+
+    printf("%d", result);
+}
+
+int main()
+{   
+    my_mul();
+    my_add();
+    my_sub();
+    my_div();
+    my_mod;
+    return 0;
+}
